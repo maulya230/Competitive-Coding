4a3af540cce407b5eaf613dc0a287a355d0ef51c
maulya230 <75597705+maulya230@users.noreply.github.com>
2023-06-17 10:19:16 +0530

    Update Sum or Product.py

--- Sum or Product.py
+++ Sum or Product.py
@@ -14,3 +14,26 @@ def sumOrProduct(n, q):
         for i in range(1,n+1):
             product=int((product*i)%MOD)
         return product
+    
+    
+    
+   #Alternate Solution
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+
+
+def sumOrProduct(n, q):
+    MOD=10**9+7
+    sum=0
+    if q==1:
+        for i in range(1,n+1):
+            sum=int((sum+i)%MOD)
+        return sum
+    elif q==2:
+        product=1
+        for i in range(1,n+1):
+            product=int((product*i)%MOD)
+        return product
